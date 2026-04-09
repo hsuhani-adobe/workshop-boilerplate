f37eee34414cfebfeb96a41616c6b7c0eabb4bff
Hajeera Suhani <hsuhani@adobe.com>
2026-04-09 15:38:01 +0530

    Updated form.js - mobile masking logic

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -581,3 +581,23 @@ export default async function decorate(block) {
     container.replaceWith(form);
   }
 }
+
+
+
+
+
+
+/**
+* Masks the first 5 digits of the mobile number with *
+* @param {*} mobileNumber
+* @returns {string} returns the mobile number with first 5 digits masked
+*/
+function maskMobileNumber(mobileNumber) {
+if (!mobileNumber) {
+return '';
+}
+const value = mobileNumber.toString();
+// Mask first 5 digits and keep the rest
+return ` ${'*'.repeat(5)}${value.substring(5)}`;
+}
+
