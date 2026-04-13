13184fed8ef4259cc21ab259d8b2091a8f3ebbec
Hajeera Suhani <hsuhani@adobe.com>
2026-04-13 12:37:08 +0530

    custom componenet

--- blocks/form/components/cards/cards.js
+++ blocks/form/components/cards/cards.js
@@ -10,13 +10,34 @@
  * @param {HTMLElement} parentElement - The parent container element of the field.
  * @param {string} formId - The unique identifier of the form.
  */
-export default async function decorate(fieldDiv, fieldJson, parentElement, formId) {
-  console.log('⚙️ Decorating cards component:', fieldDiv, fieldJson, parentElement, formId);
+// export default async function decorate(fieldDiv, fieldJson, parentElement, formId) {
+//   console.log('⚙️ Decorating cards component:', fieldDiv, fieldJson, parentElement, formId);
   
-  // TODO: Implement your custom component logic here
-  // You can access the field properties via fieldJson.properties
-  // You can access the parent container via parentElement
-  // You can access the form ID via formId
+//   // TODO: Implement your custom component logic here
+//   // You can access the field properties via fieldJson.properties
+//   // You can access the parent container via parentElement
+//   // You can access the form ID via formId
   
-  return fieldDiv;
-}
+//   return fieldDiv;
+// }
+import { createOptimizedPicture } from '../../../../scripts/aem.js';
+
+
+ export default function decorate(element, fieldJson, container, formId) {
+
+ element.classList.add('card');
+
+ element.querySelectorAll('.radio-wrapper').forEach((radioWrapper) => {
+
+ const image = createOptimizedPicture('https://main--afb--jalagari.aem.live/lab/images/media_14e602bb8ead2da119302beb16f79574bcce4995f.png', 'card-image');
+
+ radioWrapper.appendChild(image);
+
+ });
+
+ return element;
+
+ }
+
+
+
